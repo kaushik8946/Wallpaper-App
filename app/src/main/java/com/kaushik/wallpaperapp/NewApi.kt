1eb9d96fbb335d3f4da73e9b31f317e54dc46cee
Surya Kaushik <suryakaushik2003@gmail.com>
2024-01-07 11:03:49 +0530

    commit

--- app/src/main/java/com/kaushik/wallpaperapp/NewApi.kt
+++ app/src/main/java/com/kaushik/wallpaperapp/NewApi.kt
@@ -5,28 +5,37 @@ import okhttp3.OkHttpClient
 import okhttp3.Request
 
 object NewApi {
-    private val okHttpClient= OkHttpClient()
+    private val okHttpClient = OkHttpClient()
     private val builder = Request.Builder()
+
     @JvmStatic
     fun main(args: Array<String>) {
-        val urlBuilder= "https://pixabay.com/api".toHttpUrlOrNull()!!.newBuilder()
-        val params= mutableMapOf<String,String?>(
+        val urlBuilder = "https://pixabay.com/api".toHttpUrlOrNull()!!.newBuilder()
+        val params = mutableMapOf<String, String?>(
             "key" to "40393604-97e3ed091f2c7a1a29352b92e",
-            "q" to "navy"
+            "q" to "navy",
+            "orientation" to "vertical"
         )
 
         for (i in params) {
-            urlBuilder.addQueryParameter(i.key,i.value)
-
+            urlBuilder.addQueryParameter(i.key, i.value)
         }
+
         println(urlBuilder.toString())
         val url = urlBuilder.build()
         val request = builder.url(url).build()
         val response = okHttpClient.newCall(request).execute()
-        if (response.isSuccessful) {
-            println(response.body?.string())
-        } else {
-            println(response.message)
+        if (!response.isSuccessful) {
+            println("api call unsuccessfull")
+            return
         }
+        val responseString = response.body?.string()!!
+        println(responseString)
+        val responseJson = jsonRecursive(responseString)
+        println(responseJson)
+        var imagesString = responseJson["hits"].toString()
+        imagesString = imagesString.substring(1, imagesString.length - 1)
+        println(imagesString)
+        println(parseImageString(imagesString))
     }
 }
